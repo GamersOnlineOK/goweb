67ca02384544b8d29a358135f0787d7a3b27153e
Leonardo De Los Santos <leo_dls@hotmail.com>
2022-05-17 22:19:03 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,65 +1,90 @@
 $("#mc-embedded-subscribe").on("click", function (e) {
-    
-    var name=document.getElementById("mce-NOMBRE").value;
-    var email=document.getElementById("mce-EMAIL").value;
-    var telefono=document.getElementById("mce-TELEFONO").value;
-    var mensaje=document.getElementById("mce-MENSAJE").value;
-    const dataPost={nombre:name,email:email,telefono:telefono,mensaje:mensaje};
-    
-    const URL="contacto.php";
+
+    var name = document.getElementById("mce-NOMBRE").value;
+    var email = document.getElementById("mce-EMAIL").value;
+    var telefono = document.getElementById("mce-TELEFONO").value;
+    var mensaje = document.getElementById("mce-MENSAJE").value;
+    const dataPost = {
+        nombre: name,
+        email: email,
+        telefono: telefono,
+        mensaje: mensaje
+    };
+
+    const URL = "contacto.php";
     $.ajax({
         type: "POST",
         url: URL,
-        data:dataPost,
-        dataType:"json",
+        data: dataPost,
+        dataType: "json",
         success: function (response) {
             console.log(response);
             console.log("enviado");
-            
+
         }
     });
     sendSuccess(name)
-    
+
 });
 
-function sendSuccess(nombre){
+function sendSuccess(nombre) {
     $("#msgsuccess").append(`
            <h6 id="msgPrint" class="text-success" style="display:none"> Mensaje Enviado! Muchas Gracias ${nombre}</h6>
     `);
     $("#msgPrint").fadeIn(1000);
 }
 $("#msgEnviado").fadeIn(5000).delay(3000).fadeOut(1000);
-const URLser="../data/servicios.json";
+const URLser = "../data/servicios.json";
 $.ajax({
     type: "get",
     url: URLser,
-    
+
     dataType: "json",
     success: function (response) {
         for (const obj of response) {
-                        $("#serviciosItem").append(`
+            $("#serviciosItem").append(`
                         
-                        <div class="col-12 col-xl-6 card mb-3 me-3 shadow"  data-aos="fade-right">
-                            <div class="row g-0 ">
-                                <div class="col-3 col-md-2 p-2 mt-4">
-                                <img src="${obj.img}" width="100%" alt="...">
-                                </div>
-                                <div class="col-9 col-md-8">
-                                
-                                <div class="card-body d-flex align-content-between flex-column ">
-                                    
-                                        <div><h2 class="card-title">${obj.titulo}</h2></div>
-                                        <div><p class="card-text">${obj.descripcion}</p></div>
-                                        <div class="pt-3"><a href="#" class="btn btn-primary">Presupuestar</a></div>
-                                    
-                                    
+                        <div class="col-md-4  col-xs-12 "  data-aos="fade-right">
+                            <div class="row  justify-content-center border-left ">
+                                <div class="col-12 col-md-8">
+                                <div class=" d-flex align-content-center flex-column  ">
+                                        <div><h2 class="card-title text-center text-red ">${obj.titulo}</h2></div>
+                                        <div><p>${obj.descripcion}</p></div>
                                 </div>
                                 </div>
                             </div>
                         </div>   
             `);
         }
-        
+
     }
 });
+const TEAM = "../data/team.json";
+$.ajax({
+    type: "get",
+    url: TEAM,
 
+    dataType: "json",
+    success: function (response) {
+        for (const obj of response) {
+            $("#team").append(`
+                        
+                        
+                                <div class="col-8 col-md-4 d-flex flex-column mx-auto justify-content-center  p-5" data-aos="zoom-in" data-aos-delay="50">
+                                    <div class="d-flex justify-content-center">
+                                    <img style="max-height: 35vh;max-width: 50vw;" src="${obj.img}" class="img-fluid rounded-circle " alt="">
+                                    </div>
+                                
+                                    <div class="text-center align-content-center">
+                                        <div>
+                                        <h5 class="text-dark">${obj.titulo}</h5>
+                                        <p class="m-0">${obj.descripcion}</p>
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+             `);
+        }
+
+    }
+})
